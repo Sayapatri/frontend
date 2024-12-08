6859370b1a7a85a32e17c8212c2bb00c6ca657e6
Sayapatri <52479349+Sayapatri@users.noreply.github.com>
2024-12-07 20:50:51 -0500

    Update stripeCheckout.js

--- assets/js/stripeCheckout.js
+++ assets/js/stripeCheckout.js
@@ -1,7 +1,7 @@
 const stripe = Stripe('pk_test_51LYdHdD4CuJW86jL5tiIZ6EaUKsHwutlCswOjdOI3lmXxyNYGOiMDk2K3FJydN42ZyydI5S4JZ2p1SjYhyTN72ro00Hj3w9CTf');
 
 document.getElementById('checkout-button').addEventListener('click', async () => {
-    const response = await fetch('http://localhost:3000/payment/create-checkout-session', {
+    const response = await fetch('https://crypto-alert-620r.onrender.com/payment/create-checkout-session', {
         method: 'POST',
         headers: {
             'Authorization': `Bearer ${token}`,
